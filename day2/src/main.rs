8e608733faa9d16da3630cc903ca3677821ae10d
Daniel Ortiz <daniel.ortiz@partner.bmw.de>
2025-08-09 09:58:31 +0200

    Day2

---
+++ day2/src/main.rs
@@ -0,0 +1,27 @@
+mod computer;
+use computer::Computer;
+use parser::Parser;
+use std::path::Path;
+
+static FILE_PATH: &str = "day2/data/input.txt";
+
+fn main() {
+    let reseted_memory: Vec<u32> = Parser::from_txt(Path::new(FILE_PATH));
+
+    'outer: for i in 0..=99 {
+        for j in 0..=99 {
+            let mut memory_it = reseted_memory.clone();
+            memory_it[1] = i;
+            memory_it[2] = j;
+            let mut code = Computer::new(memory_it);
+            code.process();
+
+            if code.get_first_position() == 19690720 {
+                println!("i: {}", i);
+                println!("j: {}", j);
+                println!("value: {}", code.get_first_position());
+                break 'outer;
+            }
+        }
+    }
+}
---
+++ parser/src/parser.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+pub struct Parser {}
+
+impl Parser {
+    pub fn from_txt(path: &Path) -> Vec<u32> {
+        let file = File::open(path).expect("File coudlnt be opened");
+        let reader = BufReader::new(file);
+
+        let mut data = Vec::new();
+
+        for line in reader.lines() {
+            let line = line.expect("Cannot read line");
+
+            data.push(line.parse().expect("Cannot parse number"));
+        }
+
+        data
+    }
+}
